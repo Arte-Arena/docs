011e4861d84627c6d99a974479572a740688bbb8
Leandro Damasio <ldamasio@gmail.com>
2024-11-25 14:54:34 -0300

    fix: Turn classic theme

--- source/conf.py
+++ source/conf.py
@@ -4,20 +4,20 @@ project = 'Space Arena'
 copyright = '2024, Leandro Damasio'
 author = 'Leandro Damasio'
 
-extensions = [
-    'sphinx_rtd_theme',
-]
+# extensions = [
+#     'sphinx_rtd_theme',
+# ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'pt'
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_css_files = ['theme_overrides.css']
-html_theme_options = {
-    'prev_next_buttons_location': 'both',
-    'navigation_depth': 4,
-}
+html_theme = 'classic'
+# html_static_path = ['_static']
+# html_css_files = ['theme_overrides.css']
+# html_theme_options = {
+#     'prev_next_buttons_location': 'both',
+#     'navigation_depth': 4,
+# }
 
