94c151fb717c5acbe074eebf905c3fa9eae58262
Leandro Damasio <ldamasio@gmail.com>
2024-11-25 14:57:12 -0300

    fix: Turn alabaster theme

--- source/conf.py
+++ source/conf.py
@@ -1,23 +1,27 @@
-import sphinx_rtd_theme
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Space Arena'
 copyright = '2024, Leandro Damasio'
 author = 'Leandro Damasio'
 
-# extensions = [
-#     'sphinx_rtd_theme',
-# ]
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'pt'
 
-html_theme = 'classic'
-# html_static_path = ['_static']
-# html_css_files = ['theme_overrides.css']
-# html_theme_options = {
-#     'prev_next_buttons_location': 'both',
-#     'navigation_depth': 4,
-# }
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_theme = 'alabaster'
+html_static_path = ['_static']
